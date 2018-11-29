c04f707147c90d8eac42efde5f56f1c161e6dd04
Scott Francis <scotfrank28@gmail.com>
2018-11-30 00:54:44 +0300

    [Start #162327897]testing post endpoint

---
+++ app/tests/v1/test_redflags.py
@@ -0,0 +1,40 @@
+import pytest
+import json
+from ... import create_app
+from app.api.v1.models.redflagM import redflags
+
+app = create_app()
+
+
+redflag = json.dumps(
+		{
+                        "Title": "Broken bridge.", 
+                        "createdOn" : "12/12/2018",
+                        "createdBy" : "Scott francis",
+                        "type" : "red flag",
+                        "location" : "N39-098 S09-234",
+                        "status" : "Rejected",
+                        "images" : "red.jpg",
+                        "videos" : "red.mpeg",
+                        "comment" : "This is a comment",
+                         "id" : 1
+            }
+		)
+
+updates_redflag = json.dumps({
+	"status":"Rejected"
+	})
+
+
+@pytest.fixture()
+def client():
+	app = create_app()
+	test_client =app.test_client()
+	return test_client
+	
+
+def test_post(client):
+	red_post = client.post('/api/v1/redflags', data= redflag,
+                       content_type='application/json')
+	assert b'Redflag is added' in red_post.data
+	assert red_post.status_code == 201
