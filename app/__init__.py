3abe6caed32df08fc9607d241de665c43147d0a9
Scott Francis <scotfrank28@gmail.com>
2018-11-29 23:08:22 +0300

    [Start #162321682]Initial commit for the file structure

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, Blueprint
+from flask_restplus import Api, Resource
+
+from app.api.v1.views.redflagV import incident_namespace
+
+
+
+def create_app():
+
+    app = Flask(__name__)
+    api = Api(app, title="iReporter", version="1.0",
+          description=' iReporter api version 1')
+    
+   
+    api.add_namespace(incident_namespace, path='/api/v1')
+
+    
+    return app
+
+
+
+
+
+
+
+
+
+
+
+
