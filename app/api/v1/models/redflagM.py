14377f621e285c08934b5e2c12be9f34acdf1409
Scott Francis <scotfrank28@gmail.com>
2018-11-30 17:21:44 +0300

    fixing merge conflict

--- app/api/v1/models/redflagM.py
+++ app/api/v1/models/redflagM.py
@@ -4,25 +4,25 @@ from flask_restplus import Namespace, fields
 
 redflags = []
 
-# incident_namespace = Namespace('Redflag', description='Redflags v1 Endpoint')
+incident_namespace = Namespace('Redflag', description='Redflags v1 Endpoint')
 
-# flag_model = incident_namespace.model('Redflag',{
+flag_model = incident_namespace.model('Redflag',{
 
-#                     'id': fields.Integer(required=True),
-#                     'title': fields.String(required=True),
-#                     'createdOn': fields.String(required=True),
-#                     'status': fields.String(required=True),
-#                     'createdBy': fields.Integer(required=True),
-#                     'location': fields.String(required=True),
-#                     'Images': fields.Float(required=True),
-#                     'Videos': fields.String(required=True),
-#                     'comment': fields.String(required=True),
-#                     'type': fields.String(required=True)
-#                  }
-#           		)
+                    'id': fields.Integer(required=True),
+                    'title': fields.String(required=True),
+                    'createdOn': fields.String(required=True),
+                    'status': fields.String(required=True),
+                    'createdBy': fields.Integer(required=True),
+                    'location': fields.String(required=True),
+                    'Images': fields.Float(required=True),
+                    'Videos': fields.String(required=True),
+                    'comment': fields.String(required=True),
+                    'type': fields.String(required=True)
+                 }
+          		)
 
 
 
-# flag_update = incident_namespace.model('Redflag Update', {
-#    'status': fields.String
-# })
+flag_update = incident_namespace.model('Redflag Update', {
+   'status': fields.String
+})
--- app/api/v1/views/redflagV.py
+++ app/api/v1/views/redflagV.py
@@ -4,27 +4,29 @@ from flask_restplus import Resource, Namespace, fields
 from app.api.v1.models.redflagM import redflags
 from app.api.v1.models.redflagM import  redflags
 
+from app.api.v1.models.redflagM import redflags, incident_namespace, flag_model, flag_update
 
-incident_namespace = Namespace('Redflag', description='Redflags v1 Endpoint')
 
-flag_model = incident_namespace.model('Redflag',{
+# incident_namespace = Namespace('Redflag', description='Redflags v1 Endpoint')
 
-                    'id': fields.Integer(required=True),
-                    'title': fields.String(required=True),
-                    'createdOn': fields.String(required=True),
-                    'status': fields.String(required=True),
-                    'createdBy': fields.Integer(required=True),
-                    'location': fields.String(required=True),
-                    'Images': fields.String(required=True),
-                    'Videos': fields.String(required=True),
-                    'comment': fields.String(required=True),
-                    'type': fields.String(required=True)
-                 }
-                )
+# flag_model = incident_namespace.model('Redflag',{
 
-flag_update = incident_namespace.model('Redflag Update', {
-   'status': fields.String
-})
+#                     'id': fields.Integer(required=True),
+#                     'title': fields.String(required=True),
+#                     'createdOn': fields.String(required=True),
+#                     'status': fields.String(required=True),
+#                     'createdBy': fields.Integer(required=True),
+#                     'location': fields.String(required=True),
+#                     'Images': fields.String(required=True),
+#                     'Videos': fields.String(required=True),
+#                     'comment': fields.String(required=True),
+#                     'type': fields.String(required=True)
+#                  }
+#                 )
+
+# flag_update = incident_namespace.model('Redflag Update', {
+#    'status': fields.String
+# })
 
 
 @incident_namespace.route('/redflags')
